23d00af75f4c207e14403ab87a08cfe9a9ef7267
tunggulalmajid <tunggulmajid@gmail.com>
2025-12-16 00:49:08 +0700

    .

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -54,7 +54,7 @@
                     @method('POST')
 
                     <button type="submit"
-                        class="flex items-center gap-2 px-4 py-2 text-sm text-red-600 hover:bg-red-50">
+                        class="flex items-center w-full gap-2 px-4 py-2 text-sm text-red-600 hover:bg-red-50">
                         <i data-lucide="log-out" class="w-4 h-4"></i> Sign out
                     </button>
                 </form>
